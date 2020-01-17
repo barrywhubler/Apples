2a25b18d11a620450fa46bb65c47b08547285bde
Barrywhubler <super_yaro@hotmail.com>
2020-01-17 13:28:02 -0700

    First Commit

---
+++ Apples Sample/Apples SampleTests/Apples_SampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  Apples_SampleTests.swift
+//  Apples SampleTests
+//
+//  Created by Barry Hubler on 1/17/20.
+//  Copyright © 2020 Barry Hubler. All rights reserved.
+//
+
+import XCTest
+@testable import Apples_Sample
+
+class Apples_SampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
