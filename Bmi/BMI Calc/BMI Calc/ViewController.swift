1700eab52d69e39c9f6dad82c6b4094721cc5f74
barrywhubler <45104972+barrywhubler@users.noreply.github.com>
2020-01-22 11:08:26 -0700

    Add files via upload
    starting

---
+++ Bmi/BMI Calc/BMI Calc/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  BMI Calc
+//
+//  Created by Barry Hubler on 1/22/20.
+//  Copyright © 2020 Barry Hubler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var myFirstyLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
