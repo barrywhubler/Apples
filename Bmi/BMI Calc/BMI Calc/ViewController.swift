c161c433120d215790188613acd257d93798ed61
barrywhubler <45104972+barrywhubler@users.noreply.github.com>
2020-01-23 10:03:10 -0700

    Add files via upload

--- Bmi/BMI Calc/BMI Calc/ViewController.swift
+++ Bmi/BMI Calc/BMI Calc/ViewController.swift
@@ -11,11 +11,25 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var myFirstyLabel: UILabel!
     
+    var x = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        myFirstyLabel.text = "H3ll0 W0rld!"
+        
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func buttonTapped(_ sender: Any) {
+        x += 1;
+        let xtext = String(x)
+        myFirstyLabel.text = ("Button tapped " + xtext + " times!!!")
+    }
+    
+    @IBAction func reset(_ sender: Any) {
+        x = 0;
+        
+        myFirstyLabel.text = ("Button tapped 0 times!!!")
+    }
+    
 }
 
